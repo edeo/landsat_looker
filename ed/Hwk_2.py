743910177b5d9cc953e89108c20bfb8f2919edff
Ed <edobrien@gmail.com>
2014-10-10 16:45:28 -0400

    Create Hwk_2.py

---
+++ ed/Hwk_2.py
@@ -0,0 +1,11 @@
+def my_calc(x,y,z):
+        if z == "add":
+            return x + y
+        elif z =="sub":
+            return x - y
+        elif z =="mult":
+            return x*y
+        elif z == "div":
+            return x/y
+        else:
+            return x + y 
