5360fff5dff1d76c8a0ca4c975e176c3dee5444a
Heath Mills <heathmills@gmail.com>
2014-10-28 18:56:31 -0400

    updated hw #7

--- heath/hw7.py
+++ heath/hw7.py
@@ -4,6 +4,11 @@ Created on Tue Oct 28 17:14:46 2014
 
 @author: Heath
 """
+import pandas as pd
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import numpy as np
 
 """
 HW EXERCISE:
@@ -27,3 +32,18 @@ est_n.summary()
 
 # R^2 for bike-data is larger than for hourly-data
 # Bike-data is a better fit than the hourly-data
+
+"""
+4) Create a binary variable for rush hour defined by 6-9a & 4-6p
+"""
+
+#hour['rush2'] = 0
+#hour['rush2'][((hour['hr'] >= 6 & (hour['hr'] <= 9)) | ((hour['hr']) >= 16 & (hour['hr'] <= 18)))] = 1
+
+hour['rush5'] = [1 if hour_it in [6,7,8,9,16,17,18] else 0 for hour_it in hour['hr']]
+
+"""
+5) Run the regression again. Does this new variable improve the results?
+"""
+est_m = smf.ols(formula='cnt ~ temp + hum + workingday + hr + C(weathersit)', data=hours_dat).fit()
+est_m.summary()
