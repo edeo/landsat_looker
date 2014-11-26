fdd15cd91d162e15e5d0d093d538c64fb4701255
Ed <edobrien@gmail.com>
2014-11-25 20:24:47 -0500

    wrote more comments
    to explain things

--- ed/project_obrien.py
+++ ed/project_obrien.py
@@ -22,6 +22,17 @@ Or, explore 538 arcticle.
 
 import pandas as pd
 import numpy as np 
+
+'''
+The Wage and Hour division (from here on refered to as WHD) of the Department of Labor enforces minimum wage and overtime. 
+The data set I am using is all of the investitations from new york state from 2008 onward.
+I choose 2008 to try and capture some of the restuarantes that are inspected in the health data. 
+going back further risks having false matches, where a street address had a business that was investigated, but is no longer in business.
+For example, the WHD data set goes back to 1985, it is possible it would have a business in it from 1987 that had an overtime, and
+then in 2014 a new business would be in the same place. 
+
+'''
+
 nywages = pd.read_csv('whd.csv')
 nywages2 = nywages[nywages.st_cd=='NY']
 nychealth = pd.read_csv('nyc.csv')
@@ -31,6 +42,12 @@ nychealth = pd.read_csv('nyc.csv')
 nyczip= (10001, 10002, 10003, 10004, 10005, 10006, 10007, 10009, 10010, 10011, 10012, 10013, 10014, 10016, 10017, 10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 10033, 10034, 10035, 10036, 10037, 10038, 10039, 10040, 10044, 10048, 10065, 10069, 10075, 10111, 10115, 10128, 10280, 10281, 10282, 11201, 11203, 11204, 11205, 11206, 11207, 11208, 11209, 11210, 11211, 11212, 11213, 11214, 11215, 11216, 11217, 11218, 11219, 11220, 11221, 11222, 11223, 11224, 11225, 11226, 11228, 11229, 11230, 11231, 11232, 11233, 11234, 11235, 11236, 11237, 11238, 11239, 11251, 11001, 11004, 11005, 11040, 11096, 11101, 11102, 11103, 11104, 11105, 11106, 11109, 11354, 11355, 11356, 11357, 11358, 11359, 11360, 11361, 11362, 11363, 11364, 11365, 11366, 11367, 11368, 11369, 11371, 11372, 11373, 11374, 11375, 11377, 11378, 11379, 11385, 11411, 11412, 11413, 11414, 11415, 11416, 11417, 11418, 11419, 11420, 11421, 11422, 11423, 11426, 11427, 11428, 11429, 11430, 11432, 11433, 11434, 11435, 11436, 11451, 11691, 11692, 11694, 11697, 10451, 10452, 10453, 10454, 10455, 10456, 10457, 10458, 10459, 10460, 10461, 10462, 10465, 10466, 10467, 10468, 10469, 10470, 10471, 10472, 10473, 10474, 10475, 10301, 10302, 10303, 10304, 10305, 10306, 10307, 10308, 10309, 10310, 10312, 10314)
 
 nycwhd=nywages2[nywages2['zip_cd'].isin(nyczip)]
+'''
+This now gives me the wage and hour invstigations in new york city. As the data from Wage and Hour names the city differently than 
+the health data, I needed to do this to split the wage and hour data.
+'''
+
+
 #http://stackoverflow.com/questions/19936794/geocoding-a-list-of-addresses-from-a-csv-file
 # create an address column that has street , number and zipcode.
 zip = nycwhd['zip_cd'].astype(str)
